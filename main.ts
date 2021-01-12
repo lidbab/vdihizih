da9c57dca5268fa7e0c6bf6a24f74dbfc3d5fdc4
Lidija Babič <32617995+lidbab@users.noreply.github.com>
2021-01-12 19:49:30 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Diamond)
+    basic.pause(2000)
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.pause(2000)
+    basic.showIcon(IconNames.SmallSquare)
+    basic.pause(1000)
+})
